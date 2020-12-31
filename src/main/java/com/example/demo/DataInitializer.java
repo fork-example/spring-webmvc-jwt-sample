ab8c2d1771fe05a3fe386a38e96e410f8fa42ac6
leongeroge <leongeroge@outlook.com>
2020-12-31 16:32:44 +0800

    use mysql

--- src/main/java/com/example/demo/DataInitializer.java
+++ src/main/java/com/example/demo/DataInitializer.java
@@ -8,11 +8,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@ConditionalOnProperty(name = "my.database.init",havingValue = "true")
 @Component
 @Slf4j
 public class DataInitializer implements CommandLineRunner {
