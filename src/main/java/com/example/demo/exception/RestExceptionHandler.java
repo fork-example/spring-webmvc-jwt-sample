b16a032b450b143d074e08459f4bdff528f6fdfb
leongeroge <leongeroge@outlook.com>
2020-12-31 18:46:20 +0800

    refactor

--- src/main/java/com/example/demo/exception/RestExceptionHandler.java
+++ src/main/java/com/example/demo/exception/RestExceptionHandler.java
@@ -1,16 +1,20 @@
-package com.example.demo.web;
+package com.example.demo.exception;
 
+import com.example.demo.web.VehicleNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.ResponseEntity.notFound;
 
 @RestControllerAdvice
 @Slf4j
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {VehicleNotFoundException.class})
     public ResponseEntity<Void> vehicleNotFound(VehicleNotFoundException ex, WebRequest request) {
@@ -18,9 +22,15 @@ public class RestExceptionHandler {
         return notFound().build();
     }
 
+    @ExceptionHandler(value = {ResponseStatusException.class})
+    public ResponseEntity<Void> responseStatusException(ResponseStatusException ex, WebRequest request) {
+        log.debug("handling ResponseStatusException...");
+        return ResponseEntity.status(ex.getStatus()).build();
+    }
+
 //    @ExceptionHandler(value = {InvalidAuthenticationException.class})
 //    public ResponseEntity<Void> invalidJwtAuthentication(InvalidAuthenticationException ex, WebRequest request) {
 //        log.debug("handling InvalidJwtAuthenticationException...");
-//        return status(UNAUTHORIZED).build();
+//        return ResponseEntity.status(UNAUTHORIZED).build();
 //    }
 }
